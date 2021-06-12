eac0e98d77367486e94e0f5eb4c2c0c561d07a60
Hector Herrera <deux.assassin98@gmail.com>
2021-06-11 23:08:12 -0500

    Finished yacc, fixed some issues on lex and added example file

--- programa.py
+++ programa.py
@@ -1,4 +1,5 @@
 import ply.lex as lex
+import ply.yacc as yacc
 
 #################################### LEX ####################################
 
@@ -76,7 +77,6 @@ def t_VALORINT(t):
 
 def t_VALORCADENA(t):
     r'\"[^\"\n]+\"'
-    t.value = t.value.replace("\"","")
     return t
 
 def t_VALORID(t):
@@ -93,3 +93,206 @@ def t_error(t):
     t.lexer.skip(1)
 
 lexer = lex.lex()
+
+#################################### YACC ####################################
+
+precedence = (
+    ('left', 'AND', 'OR'),
+    ('left', 'VALOREQUIV', 'VALORNOEQUIV'),
+    ('nonassoc','VALORMENOR', 'VALORMEOI', 'VALORMAYOR', 'VALORMAOI'),
+    ('left', 'VALORSUMA', 'VALORRESTA'),
+    ('left', 'VALORMULT', 'VALORDIV'),
+    ('left', 'VALOREXP'),
+    ('right', 'UMINUS')
+)
+
+arbol = ()
+
+def p_inicio(p):
+    '''
+    inicio : statement
+    '''
+    global arbol
+    arbol = p[1]
+
+def p_null(p):
+    '''
+    null :
+    '''
+    pass
+
+def p_statement(p):
+    '''
+    statement : condicional statement
+              | loop_while statement
+              | loop_for statement
+              | asignacion FINAL statement
+              | log FINAL statement
+              | null
+    '''
+    if len(p) > 2:
+        if p[2] == ';':
+            p[2] = p[3]
+        p[0] = (p[1],) + p[2]
+    else:
+        p[0] = ()
+
+def p_condicional(p):
+    '''
+    condicional : if_def elseif_def else_def
+    '''
+    p[0] = ('condicional', p[1], p[2], p[3])
+
+def p_if(p):
+    '''
+    if_def : IF PARIZ expression PARDER LLAVIZ statement LLAVDER
+    '''
+    p[0] = ('if', p[3], p[6])
+
+def p_elseif(p):
+    '''
+    elseif_def : ELSEIF PARIZ expression PARDER LLAVIZ statement LLAVDER elseif_def
+               | null
+    '''
+    if len(p) > 2:
+        p[0] = (('elseif', p[3], p[6]),) + p[8]
+    else:
+        p[0] = ()
+
+def p_else(p):
+    '''
+    else_def : ELSE LLAVIZ statement LLAVDER
+             | null
+    '''
+    if len(p) > 2:
+        p[0] = ('else', p[3])
+
+def p_while(p):
+    '''
+    loop_while : WHILE PARIZ expression PARDER LLAVIZ statement LLAVDER
+               | DO LLAVIZ statement LLAVDER WHILE PARIZ expression PARDER FINAL
+    '''
+    if p[1] == "while":
+        p[0] = ('while', p[3], p[6])
+    else:
+        p[0] = ('do-while', p[7], p[3])
+
+def p_for(p):
+    '''
+    loop_for : FOR PARIZ declarar-asignar FINAL expression FINAL guardar PARDER LLAVIZ statement LLAVDER
+    '''
+    p[0] = ('for', p[3], p[5], p[7], p[10])
+
+def p_tipo(p):
+    '''
+    tipo : INT
+         | FLOAT
+         | STRING
+         | BOOL
+    '''
+    p[0] = p[1]
+
+def p_log(p):
+    '''
+    log : LOG expression
+    '''
+    p[0] = ('log', p[2])
+
+def p_asignacion(p):
+    '''
+    asignacion : declarar
+               | declarar-asignar
+               | guardar
+    '''
+    p[0] = p[1]
+
+def p_declarar(p):
+    '''
+    declarar : tipo VALORID
+    '''
+    p[0] = ('declarar', p[1], p[2])
+
+def p_guardar(p):
+    '''
+    guardar : VALORID VALORIGUAL expression
+    '''
+    p[0] = ('guardar', p[1], p[3])
+
+def p_declararAsignar(p):
+    '''
+    declarar-asignar : tipo VALORID VALORIGUAL expression
+    '''
+    p[0] = ('declarar-guardar', p[2], p[4])
+
+def p_expressionNumero(p):
+    '''
+    expression : VALORINT
+         | VALORFLOAT
+         | VALORCADENA
+         | valorbool
+    '''
+    p[0] = p[1]
+
+def p_expression_group(p):
+    '''
+    expression : PARIZ expression PARDER
+    '''
+    p[0] = p[2]
+
+def p_expression_name(p):
+    '''
+    expression : VALORID
+    '''
+    p[0] = p[1]
+
+def p_valorbool(p):
+    '''
+    valorbool : TRUE
+            | FALSE
+    '''
+    if p[1] == "true":
+        p[0] = True
+    elif p[1] == "false":
+        p[0] = False
+
+def p_expression_uminus(p):
+    '''
+    expression : VALORRESTA expression %prec UMINUS
+    '''
+    p[0] = -p[2]
+
+# Error rule for syntax errors
+def p_error(p):
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
+
+def p_expression(p):
+    '''
+    expression : expression VALORSUMA expression
+               | expression VALORRESTA expression
+               | expression VALORMULT expression
+               | expression VALORDIV expression
+               | expression VALOREXP expression
+               | expression VALOREQUIV expression
+               | expression VALORNOEQUIV expression
+               | expression VALORMENOR expression
+               | expression VALORMEOI expression
+               | expression VALORMAYOR expression
+               | expression VALORMAOI expression
+               | expression AND expression
+               | expression OR expression
+    '''
+    p[0] = ('operacion', p[1], p[2], p[3])
+
+# Build the parser
+parser = yacc.yacc()
+
+file = open("file.txt", "r")
+s = file.read()
+yacc.parse(s)
+
+print("++++++++++++++++ Arbol ++++++++++++++++\n")
+print(arbol)
+print("\n++++++++++++++++++++++++++++++++++++++\n")
